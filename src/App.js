672c3e42199c35bf3b34fbbe09891260c9b79814
unknown <somaineelam@gmail.com>
2021-09-15 20:10:06 +0530

    react basics demo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,156 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
+//import React, { useState } from 'react';
+import Person from './Person/Person.js';
+import './Person/Person.css';
+import Radium , {StyleRoot} from 'radium';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component
+{
+//const App = props => {
+//const [personStates, setPersonStates] = useState ({
+//persons : [
+//{ name: "Manu" , age: 23 },
+//{ name: "Max" , age: 28 },
+//{ name: "Neel" , age: 22 }
+//],
+//otherstate : "Some other states"
+//});
+
+state = {
+persons : [
+{ id: "abc1" , name: "Manu" , age: 23 },
+{ id: "abc2" , name: "Max" , age: 28 },
+{ id: "abc3" , name: "Neel" , age: 22 }
+],
+otherState : "Some other states",
+showPersons: false
+};
+//const switchNameHandler = () =>{
+//setPersonStates({
+//persons : [
+//{ name: "Maushi" , age: 23 },
+//{ name: "Max" , age: 28 },
+//{ name: "Neel" , age: 21 }
+//]
+//
+//});
+//}
+
+togglePersonsHandler = () => {
+const doesShow = this.state.showPersons;
+this.setState({showPersons: !doesShow});
+};
+
+nameChangeHandler = (event,id)=>{
+const personIndex = this.state.persons.findIndex(p =>{
+return p.id === id
+});
+const person = {
+...this.state.persons[personIndex]
+};
+
+person.name = event.target.value;
+const persons = [...this.state.persons];
+person[personIndex] = person;
+
+this.setState({
+persons : [
+{ id: "abc1", name: "Manu" , age: 23 },
+{ id: "abc2",name: event.target.value , age: 28 },
+{ id: "abc3" ,name: "Neel" , age: 22 }
+]
+}
+);
 }
 
-export default App;
+switchNameHandler = () =>
+{
+//console.log("Switch Clicked");
+//DONT DO THIS: this.state.persons[0].name = "Silvasa";
+this.setState({
+persons : [
+{ name: "Abc" , age: 23 },
+{ name: "Max" , age: 28 },
+{ name: "Neel" , age: 22 }
+]
+})
+};
+
+deletePersonHandler = (personIndex) =>
+{
+const persons = this.state.persons.slice();
+persons.splice(personIndex,1);
+this.setState({persons : persons});
+}
+
+render(){
+const style = {
+backgroundColor : 'green',
+color : 'white',
+font : 'inherit',
+border : '1px solid blue',
+padding : '8px',
+cursor : 'pointer',
+':hover': {
+backgroundColor : 'lightgreen',
+color : 'black',
+}
+};
+
+let persons = null;
+if (this.state.showPersons)
+{
+persons = (
+    <div>
+    {this.state.persons.map((person,index) => {
+    return <Person
+    click = {() => this.deletePersonHandler(index)}
+    name={person.name}
+    age ={person.age}
+    key ={person.id}
+    changed={(event)=>this.nameChangeHandler(event,person.id)}/>
+    })}
+    </div>
+);
+style.backgroundColor='red';
+style[':hover'] = {
+backgroundColor : 'salmon',
+color : 'black',
+}
+}
+
+const classes = [];
+if (this.state.persons.length <=2)
+{
+classes.push('red');
+}
+if (this.state.persons.length <=1)
+{
+classes.push('bold');
+}
+
+
+  return (
+  <StyleRoot>
+    <div className="App">
+      <h1>Hii Im react Developer</h1>
+      <p className = {classes.join(' ')}> This is really working </p>
+      <button style= {style} onClick={this.togglePersonsHandler}> Switch Name</button>
+      {persons}
+    </div>
+    </StyleRoot>
+  );
+//React.createElement is used to inject something
+//return React.createElement('div',{className: 'App'},React.createElement('h1',null,'Im React Developer'))
+
+}
+}
+export default Radium(App);
+// state is a property for class that extends component , state can have anything
+// useState hook of react used instead of state prop of component
+//      <Person name={this.state.persons[0].name} age ={this.state.persons[0].age} />
+//      <Person name={this.state.persons[1].name} age ={this.state.persons[1].age} click = {this.switchNameHandler.bind(this,"Max")} changed = {this.nameChangeHandler}>My Hobbies: Racing</Person>
+//      <Person name={this.state.persons[2].name} age ={this.state.persons[2].age} />
+// radium for inline hover properties and media query
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './Person.css';
+import Radium from 'radium';
 
 const person = (props) => {
 //return <p>Im a person and I am {Math.floor(Math.random()*30)} years old!</p>
+const style= {
+'@media (min-width: 500px)':{
+ width : '450px'
+}
+};
 return (
-<div className="Person">
+<div className="Person" style = {style}>
 <p onClick={props.click}>Im {props.name} and I am {props.age} years old!</p>
 <p>{props.children}</p>
 <input type="text" onChange = {props.changed} defaultValue = {props.name} />
@@ -12,9 +18,10 @@ return (
 )
 };
 
-export default person;
+export default Radium(person);
 
 // name of function with small p
 // for some dynamic data like Math.random .. add curly braces
 // export def comp to import it in app.js
 // props.children returns anything in between open and closing tag
+// for classes that extends Component .. we use this.props. and for functions directly props.
